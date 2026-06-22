dfce8a843f08fffcb0f514e4d50ff0173b3c1681
Liam <105320711+DarkTerraYT@users.noreply.github.com>
2026-06-21 20:52:58 -0400

    Primitive untested mod support

---
+++ Core/Mods/Mod.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Microsoft.Xna.Framework;
+
+namespace MinecraftClone.Core.Mods;
+
+public abstract class Mod
+{
+    internal Mod(Assembly assembly)
+    {
+        ModAssembly = assembly;
+    }
+    
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+    
+    public abstract void Initialize();
+    
+    public Assembly ModAssembly { get; }
+    
+    public virtual void EarlyUpdate(GameTime gameTime) { }
+    public virtual void Update(GameTime gameTime) { }
+    public virtual void LateUpdate(GameTime gameTime) { }
+
+    public virtual void OnUI(GameTime gameTime)
+    {
+        
+    }
+    public virtual void Draw(GameTime gameTime)
+    {
+        
+    }
+}
---
+++ Core/Mods/ModLoader.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.Xna.Framework;
+
+namespace MinecraftClone.Core.Mods;
+
+public class ModLoader
+{
+    public static readonly string ModsPath = Path.Combine(AppContext.BaseDirectory, "Mods");
+    
+    public static ModLoader Instance { get; private set; }
+
+    public IReadOnlyList<Mod> Mods => _mods;
+    private readonly List<Mod> _mods = new();
+
+    private Logger logger = new Logger("Mod Loader");
+    
+    private AssemblyLoadContext _loadContext = new ("MinecraftClone");
+    
+    public void Initialize()
+    {
+        Instance = this;
+
+        foreach (var file in Directory.GetFiles(ModsPath, "*.dll"))
+        {
+            LoadMod(file);
+        }
+    }
+
+    public void LoadMod(string file)
+    {
+        Assembly modAssembly;
+        try
+        {
+            modAssembly = _loadContext.LoadFromAssemblyPath(file);
+        }
+        catch (BadImageFormatException)
+        {
+            logger.Error(
+                $"Assembly at file {file} is not a valid .NET assembly! Please remove from the mods folder.");
+            return;
+        }
+        catch (Exception e)
+        {
+            logger.Error($"Failed to load assembly {file}: {e}");
+            return;
+        }
+
+        try
+        {
+            Type modType = modAssembly.GetTypes().FirstOrDefault(type => type.IsSubclassOf(typeof(Mod)), null);
+            if (modType == null)
+            {
+                return;
+            }
+
+            Mod mod = Activator.CreateInstance(modType, BindingFlags.NonPublic, null, [modAssembly], null) as Mod;
+            if (mod == null)
+            {
+                logger.Error($"Failed to instantiate mod from assembly {modAssembly.FullName}!");
+                return;
+            }
+
+            mod.Initialize();
+            _mods.Add(mod);
+            logger.Info($"Loaded mod {mod.Name}");
+        }
+        catch (Exception e)
+        {
+            logger.Error($"Failed to load mod {file}: {e}");
+        }
+    }
+    
+    internal void UpdateAllMods(GameTime gameTime)
+    {
+        foreach (var mod in _mods)
+        {
+            mod.EarlyUpdate(gameTime);
+        }
+        foreach (var mod in _mods)
+        {
+            mod.Update(gameTime);
+        }
+        foreach (var mod in _mods)
+        {
+            mod.LateUpdate(gameTime);
+        }
+    }
+
+    internal void DrawAllModsUi(GameTime gameTime)
+    {
+        foreach (var mod in _mods)
+        {
+            mod.OnUI(gameTime);
+        }
+    }
+    internal void DrawAllModsMesh(GameTime gameTime)
+    {
+        foreach (var mod in _mods)
+        {
+            mod.Draw(gameTime);
+        }
+    }
+}
